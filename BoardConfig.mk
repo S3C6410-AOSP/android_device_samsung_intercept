e9f951cbf45076581091ccf0e984f5d3840cf775
Littlejth <4512jth@gmail.com>
2011-04-22 10:27:47 -0500

    Merged vendor, split common files.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,31 @@ TARGET_NO_RADIOIMAGE         := true
 #TARGET_BOOTLOADER_BOARD_NAME := SPH-M910
 TARGET_BOARD_PLATFORM        := s3c6410
 
+USE_CAMERA_STUB              := false
+BOARD_USES_ECLAIR_LIBCAMERA  := true
+
+BOARD_HAS_NO_MISC_PARTITION  := true
+
+BOARD_USES_GENERIC_AUDIO     := false
+BOARD_USES_ALSA_AUDIO        := true
+BUILD_WITH_ALSA_UTILS        := true
+
+BOARD_WPA_SUPPLICANT_DRIVER  := WEXT
+WPA_SUPPLICANT_VERSION       := VER_0_6_X
+BOARD_WLAN_DEVICE            := eth0
+WIFI_DRIVER_MODULE_PATH      := "/system/libmodules/dhd.ko"
+WIFI_DRIVER_MODULE_ARG       := "firmware_path=/system/etc/wifi/bcm4329_aps.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_NAME      := "dhd"
+
+BOARD_GPS_LIBRARIES          := libsecgps libsecril-client
+
+BOARD_HAVE_BLUETOOTH         := true
+BT_USE_BTL_IF                := true
+BT_ALT_STACK                 := true
+BRCM_BTL_INCLUDE_A2DP        := true
+BRCM_BT_USE_BTL_IF           := true
+
+BUILD_PV_VIDEO_ENCODERS      := true
 # inherit from the proprietary version
 -include vendor/samsung/intercept/BoardConfigVendor.mk
 
--- intercept.mk
+++ intercept.mk
@@ -29,6 +29,179 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ## Get proprietary configuration if available
 $(call inherit-product-if-exists, vendor/samsung/intercept/intercept-vendor.mk)
 
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Intercept proprietary
+#
+
+# system/bin
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/bin/akmd2:system/bin/akmd2 \
+    vendor/samsung/intercept/prebuilt/bin/BCM4329B1_002.002.023.0417.0421.hcd:system/bin/BCM4329B1_002.002.023.0417.0421.hcd \
+    vendor/samsung/intercept/prebuilt/bin/bluetoothd:system/bin/bluetoothd \
+    vendor/samsung/intercept/prebuilt/bin/iqmsd:system/bin/iqmsd \
+    vendor/samsung/intercept/prebuilt/bin/mfgloader:system/bin/mfgloader \
+    vendor/samsung/intercept/prebuilt/bin/playlogo:system/bin/playlogo \
+    vendor/samsung/intercept/prebuilt/bin/pppd:system/bin/pppd \
+    vendor/samsung/intercept/prebuilt/bin/pppd_runner:system/bin/pppd_runner \
+    vendor/samsung/intercept/prebuilt/bin/rilclient-test:system/bin/rilclient-test \
+    vendor/samsung/intercept/prebuilt/bin/rild:system/bin/rild \
+    vendor/samsung/intercept/prebuilt/bin/wlservice:system/bin/wlservice \
+    vendor/samsung/intercept/prebuilt/bin/wpa_supplicant:system/bin/wpa_supplicant
+
+# system/etc
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/etc/audio/aeqcoe.txt:system/etc/audio/aeqcoe.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/eqfilter.txt:system/etc/audio/eqfilter.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/lmfilter.txt:system/etc/audio/lmfilter.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/situation.txt:system/etc/audio/situation.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/soundbooster.txt:system/etc/audio/soundbooster.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/stream_earpiece.txt:system/etc/audio/stream_earpiece.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/stream_headset.txt:system/etc/audio/stream_headset.txt \
+    vendor/samsung/intercept/prebuilt/etc/audio/stream_speaker.txt:system/etc/audio/stream_speaker.txt \
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/etc/wifi/bcm4329_aps.bin:system/etc/wifi/bcm4329_aps.bin \
+    vendor/samsung/intercept/prebuilt/etc/wifi/bcm4329_mfg.bin:system/etc/wifi/bcm4329_mfg.bin \
+    vendor/samsung/intercept/prebuilt/etc/wifi/bcm4329_sta.bin:system/etc/wifi/bcm4329_sta.bin \
+    vendor/samsung/intercept/prebuilt/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    vendor/samsung/intercept/prebuilt/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    vendor/samsung/intercept/prebuilt/etc/wifi/wifi.conf:system/etc/wifi/wifi.conf \
+    vendor/samsung/intercept/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/etc/ppp/chap-secrets:system/etc/ppp/chap-secrets \
+    vendor/samsung/intercept/prebuilt/etc/ppp/ip-down:system/etc/ppp/ip-down \
+    vendor/samsung/intercept/prebuilt/etc/ppp/ip-up:system/etc/ppp/ip-up \
+    vendor/samsung/intercept/prebuilt/etc/ppp/options:system/etc/ppp/options \
+    vendor/samsung/intercept/prebuilt/etc/ppp/pap-secrets:system/etc/ppp/pap-secrets \
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/samsung/intercept/prebuilt/etc/asound.conf:system/etc/asound.conf \
+    vendor/samsung/intercept/prebuilt/etc/cdma-carriers-conf.xml:system/etc/cdma-carriers-conf.xml \
+    vendor/samsung/intercept/prebuilt/etc/gps.conf:system/etc/gps.conf \
+    vendor/samsung/intercept/prebuilt/etc/init.cdma-pppd:system/etc/init.cdma-pppd \
+    vendor/samsung/intercept/prebuilt/etc/init.gprs-pppd:system/etc/init.gprs-pppd \
+    vendor/samsung/intercept/prebuilt/etc/poweron.snd:system/etc/poweron.snd \
+    vendor/samsung/intercept/prebuilt/etc/vold.fstab:system/etc/vold.fstab
+
+# system/lib
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+    vendor/samsung/intercept/prebuilt/lib/egl/libEGL_fimg.so:system/lib/egl/libEGL_fimg.so \
+    vendor/samsung/intercept/prebuilt/lib/egl/libGLESv1_CM_fimg.so:system/lib/egl/libGLESv1_CM_fimg.so \
+    vendor/samsung/intercept/prebuilt/lib/egl/libGLESv2_fimg.so:system/lib/egl/libGLESv2_fimg.so
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/lib/hw/copybit.SPH-M910.so:system/lib/hw/copybit.SPH-M910.so \
+    vendor/samsung/intercept/prebuilt/lib/hw/gralloc.SPH-M910.so:system/lib/hw/gralloc.SPH-M910.so \
+    vendor/samsung/intercept/prebuilt/lib/hw/lights.SPH-M910.so:system/lib/hw/lights.SPH-M910.so \
+    vendor/samsung/intercept/prebuilt/lib/hw/sensors.SPH-M910.so:system/lib/hw/sensors.SPH-M910.so
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/lib/libcamera.so:system/lib/libcamera.so \
+    vendor/samsung/intercept/prebuilt/lib/libChunkAlloc.so:system/lib/libChunkAlloc.so \
+    vendor/samsung/intercept/prebuilt/lib/libril.so:system/lib/libril.so \
+    vendor/samsung/intercept/prebuilt/lib/libs3c2drender.so:system/lib/libs3c2drender.so \
+    vendor/samsung/intercept/prebuilt/lib/libs3cjpeg.so:system/lib/libs3cjpeg.so \
+    vendor/samsung/intercept/prebuilt/lib/libs263domxoc.so:system/lib/libs263domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libs263eomxoc.so:system/lib/libs263eomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libs264domxoc.so:system/lib/libs264domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsaacdomxoc.so:system/lib/libsaacdomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsac3domxoc.so:system/lib/libsac3domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamrdomxoc.so:system/lib/libsamrdomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamreomxoc.so:system/lib/libsamreomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libSamsungAFConverter.so:system/lib/libSamsungAFConverter.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamsunglmeq.so:system/lib/libsamsunglmeq.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamsungr2vs.so:system/lib/libsamsungr2vs.so \
+    vendor/samsung/intercept/prebuilt/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
+    vendor/samsung/intercept/prebuilt/lib/libsavidocn.so:system/lib/libsavidocn.so \
+    vendor/samsung/intercept/prebuilt/lib/libsavidreg.so:system/lib/libsavidreg.so \
+    vendor/samsung/intercept/prebuilt/lib/libsavscmn.so:system/lib/libsavscmn.so \
+    vendor/samsung/intercept/prebuilt/lib/libsecgps.so:system/lib/libsecgps.so \
+    vendor/samsung/intercept/prebuilt/lib/libsec-ril40.so:system/lib/libsec-ril40.so \
+    vendor/samsung/intercept/prebuilt/lib/libsecril-client.so:system/lib/libsecril-client.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp3domxoc.so:system/lib/libsmp3domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp4fmocn.so:system/lib/libsmp4fmocn.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp4vdomxoc.so:system/lib/libsmp4vdomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp4veomxoc.so:system/lib/libsmp4veomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsrec_jni.so:system/lib/libsrec_jni.so \
+    vendor/samsung/intercept/prebuilt/lib/libsvc1domxoc.so:system/lib/libsvc1domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsvidmiooc.so:system/lib/libsvidmiooc.so \
+    vendor/samsung/intercept/prebuilt/lib/libswmadomxoc.so:system/lib/libswmadomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libswmfdocn.so:system/lib/libswmfdocn.so \
+    vendor/samsung/intercept/prebuilt/lib/libswmfdreg.so:system/lib/libswmfdreg.so \
+    vendor/samsung/intercept/prebuilt/lib/libwlservice.so:system/lib/libwlservice.so \
+    vendor/samsung/intercept/prebuilt/lib/libwpa_client.so:system/lib/libwpa_client.so
+
+#Copy modules to /obj/lib for build dependencies
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/lib/libcamera.so:obj/lib/libcamera.so \
+    vendor/samsung/intercept/prebuilt/lib/libChunkAlloc.so:obj/lib/libChunkAlloc.so \
+    vendor/samsung/intercept/prebuilt/lib/libril.so:obj/lib/libril.so \
+    vendor/samsung/intercept/prebuilt/lib/libs3c2drender.so:obj/lib/libs3c2drender.so \
+    vendor/samsung/intercept/prebuilt/lib/libs3cjpeg.so:obj/lib/libs3cjpeg.so \
+    vendor/samsung/intercept/prebuilt/lib/libs263domxoc.so:obj/lib/libs263domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libs263eomxoc.so:obj/lib/libs263eomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libs264domxoc.so:obj/lib/libs264domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsaacdomxoc.so:obj/lib/libsaacdomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsac3domxoc.so:obj/lib/libsac3domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamrdomxoc.so:obj/lib/libsamrdomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamreomxoc.so:obj/lib/libsamreomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libSamsungAFConverter.so:obj/lib/libSamsungAFConverter.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamsunglmeq.so:obj/lib/libsamsunglmeq.so \
+    vendor/samsung/intercept/prebuilt/lib/libsamsungr2vs.so:obj/lib/libsamsungr2vs.so \
+    vendor/samsung/intercept/prebuilt/lib/lib_Samsung_Resampler.so:obj/lib/lib_Samsung_Resampler.so \
+    vendor/samsung/intercept/prebuilt/lib/libsavscmn.so:obj/lib/libsavscmn.so \
+    vendor/samsung/intercept/prebuilt/lib/libsecgps.so:obj/lib/libsecgps.so \
+    vendor/samsung/intercept/prebuilt/lib/libsecril-client.so:obj/lib/libsecril-client.so \
+    vendor/samsung/intercept/prebuilt/lib/libsec-ril40.so:obj/lib/libsec-ril40.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp3domxoc.so:obj/lib/libsmp3domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp4fmocn.so:obj/lib/libsmp4fmocn.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp4vdomxoc.so:obj/lib/libsmp4vdomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsmp4veomxoc.so:obj/lib/libsmp4veomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsrec_jni.so:obj/lib/libsrec_jni.so \
+    vendor/samsung/intercept/prebuilt/lib/libsvc1domxoc.so:obj/lib/libsvc1domxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libsvidmiooc.so:obj/lib/libsvidmiooc.so \
+    vendor/samsung/intercept/prebuilt/lib/libswmadomxoc.so:obj/lib/libswmadomxoc.so \
+    vendor/samsung/intercept/prebuilt/lib/libswmfdocn.so:obj/lib/libswmfdocn.so \
+    vendor/samsung/intercept/prebuilt/lib/libswmfdreg.so:obj/lib/libswmfdreg.so \
+    vendor/samsung/intercept/prebuilt/lib/libwlservice.so:obj/lib/libwlservice.so \
+    vendor/samsung/intercept/prebuilt/lib/libwpa_client.so:obj/lib/libwpa_client.so
+
+
+# system/libmodules
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/libmodules/dhd.ko:system/libmodules/dhd.ko
+
+
+# system/media
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/media/bootani.qmg:system/media/bootani.qmg
+
+
+# system/xbin
+PRODUCT_COPY_FILES += \
+    vendor/samsung/intercept/prebuilt/xbin/ipcdump:system/xbin/ipcdump \
+    vendor/samsung/intercept/prebuilt/xbin/ipctool:system/xbin/ipctool
+
 # Install the features available on this device.
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
@@ -47,3 +220,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/samsung/intercept/prebuilt/app/linpack.apk:system/app/linpack.apk \
     device/samsung/intercept/prebuilt/app/Superuser.apk:system/app/Superuser.apk
+
+# Call generic 6410 makefiles 
+$(call inherit-product-if-exists, device/samsung/common/common.mk)
